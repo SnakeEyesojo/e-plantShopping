fc928dbf75e5c37477192ffd9de5a3886c62eaff
SnakeEyesojo <snake.eyes.ojo@gmail.com>
2025-02-03 16:13:09 +1100

    Fixed routing issue for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './store.js';
+import { HashRouter } from 'react-router-dom';  // Agrega esta línea
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
-)
+);
